2327178d4a7c10b9a2115582115385bbce53fd6e
MAZOUZ Wajih <wajih.mazouz@gmail.com>
2025-09-28 18:50:57 +0200

    Ajout MainOutputTest: teste que la méthode main affiche les infos joueur, niveau, capacités et inventaire

---
+++ src/test/java/re/forestier/edu/MainOutputTest.java
@@ -0,0 +1,30 @@
+package re.forestier.edu;
+
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+
+class MainOutputTest {
+
+    @Test
+    void main_prints_expected_information() {
+        PrintStream originalOut = System.out;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        try {
+            Main.main(new String[]{});
+            String printed = out.toString();
+
+            assertThat(printed, containsString("Joueur"));
+            assertThat(printed, containsString("Niveau :"));
+            assertThat(printed, containsString("Capacités :"));
+            assertThat(printed, containsString("Inventaire :"));
+            assertThat(printed, containsString("------------------"));
+        } finally {
+            System.setOut(originalOut);
+        }
+    }
+}
+
